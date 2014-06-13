48be25dfd49faec50a37fbb59543264947b3dc43
huberlv <357370736@qq.com>
2014-06-13 09:25:47 +0800

    c
    c

--- bg.js
+++ bg.js
@@ -2,6 +2,7 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
 	if(request.cmd=='displayUpdate'){
+
 		chrome.browserAction.setBadgeText({text:request.updateNum});
 		
 	}
