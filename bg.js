5eda4a1be67046ba65d1d2fcf9380a61ef9cdb84
huberlv <357370736@qq.com>
2014-06-18 00:28:38 +0800

    修改已读功能，改为html5本地存储
    修改已读功能，改为html5本地存储

--- bg.js
+++ bg.js
@@ -2,9 +2,13 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
 	if(request.cmd=='displayUpdate'){
-
 		chrome.browserAction.setBadgeText({text:request.updateNum});
 		
 	}
 });
+//chrome.tabs.create({"url":"http://www.baidu.com","selected":false},function(tab){alert(tab);});
+//chrome.windows.create({"url":"http://www.baidu.com","width":1,"height":1},function(w){alert(w.location.href);});
+//chrome.tabs.create({'url':'http://localhost:8080/personalfocus/index.jsp','selected':false,'pinned':true,'index':0},function(tab){} );
+window.showModalDialog("http://localhost:8080/personalfocus/index.jsp",window);
+//window.open('http://localhost:8080/personalfocus/index.jsp','_blank','height=0,width=0,top=0,left=0,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no,alwaysLowered=no,z-look=no')
   
--- content.js
+++ content.js
@@ -1,46 +1,110 @@
+//是否对比链接 fixme 需入从配置读取
+var compareLink=true;
+var backgroundColor="rgb(0, 255, 0)";
 
+if(!window.localStorage){
+	alert('This browser does NOT support localStorage');
+}
+//突出显示更新内容 fixme 需入从配置读取
 function displyUpdate(dom){
 	// dom.style.color="#FF0000";
-	// dom.style.textDecoration="overline";
-	 dom.style.backgroundColor="#00FF00";
+	 //dom.style.textDecoration="overline";
+	 dom.style.backgroundColor_s=dom.style.backgroundColor;
+	 dom.style.backgroundColor=backgroundColor;
 }
-chrome.storage.sync.get(personalFocus_oldLinks,function(valueArray) {
 
-});
+function restUpdate(dom){
+	// dom.style.color="#FF0000";
+	 //dom.style.textDecoration="overline";
+	if(dom.style.backgroundColor==backgroundColor){
+	   dom.style.backgroundColor=dom.style.backgroundColor_s==undefined?"":dom.style.backgroundColor_s;
+	}
+}
 
-//chrome.storage.local.clear(function() { alert('清除全部存储项成功')       });
-var personalFocus_oldLinks=encodeURIComponent("personalFocus_oldLinks:"+window.location.href);
-chrome.storage.local.get(personalFocus_oldLinks, function(valueArray) {
+//清除本地存储 fixme 需要实现事件
+function clear(){
+	localStorage.clear();
+}
+
+//链接的key
+var personalFocus_oldContent=encodeURIComponent("personalFocus_oldContent:"+window.location.href);
+var personalFocus_oldContent_temp=encodeURIComponent("personalFocus_oldContent_temp:"+window.location.href);
+//主方法，对比网页内容
+function compare() {
         console.log("*****start personalFocus******"); 	 
-		var oldLinks=valueArray[personalFocus_oldLinks];
-		var links=document.body.getElementsByTagName("a");
-		var newLinks=new Object();
+		var oldContenttr=localStorage.getItem(personalFocus_oldContent);
+
 		var exists=false;
 
-		if(oldLinks!=undefined){
+		if(oldContenttr!=undefined){
 			exists=true;
 		}
+		var oldContent=JSON.parse(''+oldContenttr);
 		console.log("visited:"+exists);
-		console.log("oldLinks:"+oldLinks);
+		console.log("oldContent:"+oldContent);
+		var content=getContentToCompare();
+		var newContent=new Object();
+		
 		var updateNum=0;
-		for(var i=0;i<links.length;i++){
-		     newLinks[links[i].innerText]="";
+		for(var i=0;i<content.length;i++){
+			 var compareContent=content[i].innerText;
+			 compareContent=compareLink?compareContent+(content[i].href==undefined?"":content[i].href):compareContent;
+		     newContent[compareContent]="";
 			 if(exists){
-                 if( oldLinks[links[i].innerText]==undefined){ 
-					 displyUpdate(links[i]);
+                 if( oldContent[compareContent]==undefined){ 
+					 displyUpdate(content[i]);
 					 updateNum++;
 				 }
 			 }
 		}
-		var storage=new Object();
-		storage[personalFocus_oldLinks]=newLinks;
-		chrome.storage.local.set(storage, function() {
-             console.log('storage success!')       
-		});
-		 console.log("*****end personalFocus******"); 	  
+		localStorage.setItem(personalFocus_oldContent_temp,JSON.stringify(newContent));
+		if(!exists){
+			read();
+		}
+		console.log("*****end personalFocus******"); 	  
+		sendUpdateMessage(updateNum);
+}
+/**
+ * 获取需要对比的dom节点
+ * @param select
+ * @returns
+ */
+function getContentToCompare(){
+	var select="";
+	var content=null;
+	if(select==""){
+		content=document.body.getElementsByTagName("a");
+	}
+	
+	return content;
+}
 
-		chrome.extension.sendMessage({cmd: "displayUpdate",'updateNum':updateNum+""},function(response) {
-			 console.log("content response:"+response); 	  
-		});
-});
+function read(){
+	var temp=localStorage.getItem(personalFocus_oldContent_temp);
+	if(temp!=null){
+		localStorage.setItem(personalFocus_oldContent,temp);
+	}
+	var content=getContentToCompare();
+	for(var i=0;i<content.length;i++){
+		restUpdate(content[i]);
+	}
+	
+}
+function sendUpdateMessage(updateNum){
+	chrome.extension.sendMessage({cmd: "displayUpdate",'updateNum':updateNum+""},function(response) {
+		 console.log("content response:"+response); 	  
+	});
+}
+compare();
+//当窗口重新选取时再对比内容
+var sourceOnfocus=window.sourceOnfocus;
+window.onfocus=function() {
+    compare();
+    if(sourceOnfocus!=undefined){
+    	sourceOnfocus();
+    }
+}
+//***********fixme 需要实现事件
+document.body.onclick=read;
 
+alert(window.opener);
